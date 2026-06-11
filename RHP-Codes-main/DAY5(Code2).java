74f892a61531011d95f0c68bdf54ca03d10e6443
Sujay Paul <sujaypaul2711@gmail.com>
2026-06-11 11:55:47 +0530

    Added solution

--- RHP-Codes-main/DAY5(Code2).java
+++ RHP-Codes-main/DAY5(Code2).java
@@ -1,3 +1,28 @@
-public class DAY5(Code2) {
-    
+
+                    //Largest subarray of 0's and 1's
+
+class Solution {
+    public int maxLen(int[] arr) {
+        // Your code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum =0;
+        int maxlength=0;
+        map.put(0,-1);
+        for(int i=0 ;i<arr.length;i++)
+        {
+            if(arr[i]==0)
+            {
+                sum+=1;
+            }else{
+                sum+=-1;
+            }
+            if(map.containsKey(sum))
+            {
+                maxlength=Math.max(maxlength,i-(map.get(sum)));
+            }else{
+                map.put(sum,i);
+            }
+        }
+        return maxlength;
+    }
 }
