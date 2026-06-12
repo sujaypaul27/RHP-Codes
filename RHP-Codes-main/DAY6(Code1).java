30d58ab6f720a196a9077a4d6f1e970fc0416174
Sujay Paul <sujaypaul2711@gmail.com>
2026-06-12 11:45:39 +0530

    Added code 1 sol

--- RHP-Codes-main/DAY6(Code1).java
+++ RHP-Codes-main/DAY6(Code1).java
@@ -1,3 +1,41 @@
-public class DAY6(Code1) {
-    
+// problem link : https://atcoder.jp/contests/abc460/tasks/abc460_c
+//question :C - Sushi 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        long[] i=new long[m];
+        long[] j=new long[n];
+        long counter=0;
+        for(int k = 0; k < m; k++)
+         {
+             i[k] = sc.nextLong();
+        }
+
+        for(int k = 0; k < n; k++)
+        {
+             j[k] = sc.nextLong();
+        }
+        Arrays.sort(j);
+        Arrays.sort(i);
+        int up=m-1;
+        int down=n-1;
+        while(up>=0 && down>=0)
+        {
+           if(j[down]<=(2*i[up]))
+           {
+             counter++;
+             down--;
+             up--;
+           }else{
+           down--;
+               
+           }
+        }
+        System.out.println(counter);
+    }
 }
